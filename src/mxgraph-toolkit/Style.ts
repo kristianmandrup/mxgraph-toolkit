7722825983ca434ea93e2fb719236590dcfb1556
Kristian Mandrup <kmandrup@gmail.com>
2020-05-08 19:50:32 +0100

    improve style API

--- src/mxgraph-toolkit/Style.ts
+++ src/mxgraph-toolkit/Style.ts
@@ -95,7 +95,6 @@ export class StyleCombiner {
     return style.split('=')[1]
   }
 
-
   stylesToMap(styles): any {
     return styles.reduce((acc, style) => {
       const name = this.nameOf(style)
@@ -104,7 +103,6 @@ export class StyleCombiner {
       return acc
     })
   }
-
 }
 
 
@@ -112,35 +110,129 @@ export class Style {
   _strokeColor: string = ''
   _edgeStyle: string = ''
   _fillColor: string = ''
+  _shape: string = ''
+  _image: string = ''
+  _verticalLabelPosition: string = ''
+  _verticalAlign: string = ''
+  _imageBackground: string = ''
+  _imageBorder: string = ''
+  _startSize: number = 12
+  _noLabel: number = 12
+  _labelPosition: string = ''
+
 
   constructor({strokeColor, edgeStyle, fillColor}: IStyleArgs) {
-    this.strokeColor = strokeColor || ''
-    this.edgeStyle = edgeStyle || ''
-    this.fillColor = fillColor || ''
+    this.setStrokeColor(strokeColor) 
+    this.setEdgeStyle(edgeStyle)
+    this.setFillColor(fillColor)
   }
 
-  set strokeColor(color: string) {
+  setStrokeColor(color: string = '') {
     this._strokeColor = color
+    return this
   }
 
   get strokeColor(): string {
     return this._strokeColor
   }
 
-  set edgeStyle(style: string) {
+  setEdgeStyle(style: string = '') {
     this._edgeStyle = style
+    return this
   }
 
   get edgeStyle() {
     return this._edgeStyle
   }
 
-  set fillColor(color: string) {
+  setFillColor(color: string = '') {
     this._fillColor = color
+    return this
   }
 
   get fillColor() {
     return this._fillColor
+  }
+
+  setShape(shape: string = '') {
+    this._shape = shape
+    return this
+  }
+
+  get shape() {
+    return this._shape
+  }
+
+  setImage(image: string = '') {
+    this._image = image
+    return this
+  }
+
+  get image() {
+    return this._image
+  }
+
+  setVerticalLabelPosition(pos: string = '') {
+    this._verticalLabelPosition = pos
+    return this
+  }
+
+  get verticalLabelPosition() {
+    return this._verticalLabelPosition
+  }
+
+  setVerticalAlign(align: string = '') {
+    this._verticalAlign = align
+    return this
+  }
+
+  get verticalAlign() {
+    return this._verticalAlign
+  }
+
+  setImageBackground(bckgrnd: string = '') {
+    this._imageBackground = bckgrnd
+    return this
+  }
+
+  get imageBackground() {
+    return this._imageBackground
+  }
+
+  setImageBorder(border: string = '') {
+    this._imageBorder = border
+    return this
+  }
+
+  get imageBorder() {
+    return this._imageBorder
+  }
+
+  setStartSize(size: number) {
+    this._startSize = size
+    return this
+  }
+
+  get startSize() {
+    return this._startSize
+  }
+
+  setNoLabel(value: number) {
+    this._noLabel = value
+    return this
+  }
+
+  get noLabel(): number {
+    return this._noLabel
+  }
+  
+  setLabelPosition(pos: string) {
+    this._labelPosition = pos
+    return this
+  }
+
+  get labelPosition(): string {
+    return this._labelPosition
   }
 
   get styles() {
