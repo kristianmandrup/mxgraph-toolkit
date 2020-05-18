70e31284800ef5573c6d3ee2001538d804ba607c
Kristian Mandrup <kmandrup@gmail.com>
2020-05-18 20:54:27 +0100

    fixed some Menu classes

--- src/ui/graph/table/TableLayout.ts
+++ src/ui/graph/table/TableLayout.ts
@@ -1,112 +1,108 @@
-import mx from "mx";
-const { mxGraphLayout } = mx;
+import mx from 'mx'
+const { mxGraphLayout } = mx
 
 /**
  * Table Layout
  */
 export class TableLayout {
-  graph: any;
+  graph: any
 
   constructor(graph) {
     // inherits from mxGraphLayout
     // @ts-ignore
-    mxGraphLayout.call(this, graph);
-    this.graph = graph;
+    mxGraphLayout.call(this, graph)
+    this.graph = graph
   }
 
   /**
- * Reorders rows.
- */
+   * Reorders rows.
+   */
   moveCell(cell, x, y) {
     // TODO: Reorder rows
-    console.log("tableLayout.moveCell", cell, x, y);
+    console.log('tableLayout.moveCell', cell, x, y)
   }
 
   /**
- * 
- */
+   *
+   */
   resizeCell(cell, geo, prev) {
     if (this.graph.isTable(cell)) {
-      this.graph.tableResized(cell, geo, prev);
+      this.graph.tableResized(cell, geo, prev)
     } else if (this.graph.isTableRow(cell)) {
-      this.graph.tableRowResized(cell, geo, prev);
+      this.graph.tableRowResized(cell, geo, prev)
     } else if (this.graph.isTableCell(cell)) {
-      this.graph.tableCellResized(cell, geo, prev);
+      this.graph.tableCellResized(cell, geo, prev)
     }
-  } /**
- * Updates column width and row height.
- */
-
+  }
+  /**
+   * Updates column width and row height.
+   */
   execute(table) {
-    var off = this.graph.getActualStartSize(table, true);
-    var model = this.graph.getModel();
-    var y = off.y;
-    var rows: any[] = [];
-    var maxX = 0;
-    var x = 0;
-
-    console.log("tableLayout.execute", table, off);
-
+    var off = this.graph.getActualStartSize(table, true)
+    var model = this.graph.getModel()
+    var y = off.y
+    var rows: any[] = []
+    var maxX = 0
+    var x = 0
     for (var i = 0; i < model.getChildCount(table); i++) {
-      var row = model.getChildAt(table, i);
+      var row = model.getChildAt(table, i)
 
       if (row != null && model.isVertex(row)) {
-        var rowGeo = this.graph.getCellGeometry(row);
+        var rowGeo = this.graph.getCellGeometry(row)
 
         if (rowGeo != null) {
-          rowGeo = rowGeo.clone();
-          var rowOff = this.graph.getActualStartSize(row, true);
-          var childCount = model.getChildCount(row);
-          x = rowOff.x;
+          rowGeo = rowGeo.clone()
+          var rowOff = this.graph.getActualStartSize(row, true)
+          var childCount = model.getChildCount(row)
+          x = rowOff.x
 
           for (var j = 0; j < childCount; j++) {
-            var cell = model.getChildAt(row, j);
+            var cell = model.getChildAt(row, j)
 
             if (cell != null) {
-              var geo = this.graph.getCellGeometry(cell);
+              var geo = this.graph.getCellGeometry(cell)
 
               if (geo != null) {
-                geo = geo.clone();
+                geo = geo.clone()
 
-                rowGeo.height = geo.height +
-                  rowOff.y + rowOff.height;
-                geo.x = x;
-                geo.y = rowOff.y;
-                model.setGeometry(cell, geo);
+                rowGeo.height = geo.height + rowOff.y + rowOff.height
+                geo.x = x
+                geo.y = rowOff.y
+                model.setGeometry(cell, geo)
 
-                x += geo.width;
+                x += geo.width
 
                 if (j == childCount - 1) {
-                  rows.push([geo, rowGeo, rowOff]);
-                  maxX = Math.max(x, maxX);
+                  rows.push([geo, rowGeo, rowOff])
+                  maxX = Math.max(x, maxX)
                 }
               }
             }
           }
 
-          rowGeo.width = x + rowOff.width;
-          rowGeo.y = y;
-          rowGeo.x = off.x;
-          model.setGeometry(row, rowGeo);
+          rowGeo.width = x + rowOff.width
+          rowGeo.y = y
+          rowGeo.x = off.x
+          model.setGeometry(row, rowGeo)
 
-          y += rowGeo.height;
+          y += rowGeo.height
         }
       }
     }
 
     // Updates table size
-    var tableGeo = this.graph.getCellGeometry(table);
+    var tableGeo = this.graph.getCellGeometry(table)
 
     if (tableGeo != null) {
-      tableGeo = tableGeo.clone();
-      tableGeo.width = x + off.x + off.width;
-      tableGeo.height = y + off.height;
-      model.setGeometry(table, tableGeo);
+      tableGeo = tableGeo.clone()
+      tableGeo.width = x + off.x + off.width
+      tableGeo.height = y + off.height
+      model.setGeometry(table, tableGeo)
 
       for (var i = 0; i < rows.length; i++) {
         if (rows[i][1].width < maxX) {
-          rows[i][0].width += maxX - rows[i][1].width;
-          rows[i][1].width += maxX - rows[i][1].width;
+          rows[i][0].width += maxX - rows[i][1].width
+          rows[i][1].width += maxX - rows[i][1].width
         }
       }
     }
--- src/ui/menus/Menu.ts
+++ src/ui/menus/Menu.ts
@@ -1,38 +1,47 @@
-mxEventObject
+import mx from 'mx'
+const { mxEventObject } = mx
+
 /**
  * Constructs a new action for the given parameters.
  */
 export class Menu {
+  funct: any
+  enabled: any
+
   // Menu inherits from mxEventSource
   // mxUtils.extend(Menu, mxEventSource);
 
   constructor(funct, enabled?) {
     // mxEventSource.call(this);
-    this.funct = funct;
-    this.enabled = (enabled != null) ? enabled : true;
-  };
+    this.funct = funct
+    this.enabled = enabled != null ? enabled : true
+  }
 
   /**
    * Sets the enabled state of the action and fires a stateChanged event.
    */
   isEnabled() {
-    return this.enabled;
-  };
+    return this.enabled
+  }
 
   /**
    * Sets the enabled state of the action and fires a stateChanged event.
    */
   setEnabled(value) {
     if (this.enabled != value) {
-      this.enabled = value;
-      this.fireEvent(new mxEventObject('stateChanged'));
+      this.enabled = value
+      this.fireEvent('stateChanged')
     }
-  };
+  }
+
+  fireEvent(event) {
+    // this.fireEvent(new mxEventObject('stateChanged'))
+  }
 
   /**
    * Sets the enabled state of the action and fires a stateChanged event.
    */
   execute(menu, parent) {
-    this.funct(menu, parent);
-  };
+    this.funct(menu, parent)
+  }
 }
